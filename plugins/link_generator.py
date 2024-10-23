fa19e64464ecd5914bcf5c8b64f0b801d041c240
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-10-24 01:02:48 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -40,7 +40,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://tamilserialbot.jasurun.workers.dev?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>code : \n<code>{base64_string}</code></b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"🔗 ➟ {link}", quote=True, reply_markup=reply_markup)
 
 
 
@@ -62,4 +62,4 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://tamilserialbot.jasurun.workers.dev?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>{file_name} ~ <code>{base64_string}</code> - {file_size}\n\n{link}</b>", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>📺 ➟ {file_name}\n\n⏰ ➟ {duration}\n\n🔎 ➟ {file_size}\n\n🔗 ➟ {link}</b>", quote=True, reply_markup=reply_markup)
