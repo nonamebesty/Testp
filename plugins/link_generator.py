9cb2f539e138450403fe5e027bb740ba13c262e2
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-10-27 08:06:37 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -62,4 +62,4 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://tamilserialbot.jasurun.workers.dev?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>★○★○★○★\n{file_name} - {➤ {duration}} - 🌺 {file_size}\n\n{link}\n★○★○★○★</b>", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>★○★○★○★\n{file_name} - {🌺 {duration}} - 🌺 {file_size}\n\n{link}\n★○★○★○★</b>", quote=True, reply_markup=reply_markup)
