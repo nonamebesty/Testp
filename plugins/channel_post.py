15f46ae983aa41b167db38e45c7bd2f9c71d663d
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-10-27 11:58:53 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -3,7 +3,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 import random
-from bot import Bot
+from bot import Bot, emoji
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, USER_REPLY_TEXT
 from helper_func import encode
 
