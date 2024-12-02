5f6a15d52594256b684752231e9895f88e0fe0a2
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-12-03 03:15:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #force user to join your backup channel leave 0 if you don't need.
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002092476960"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002316896807"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002133392581"))
 
 if FORCE_SUB_CHANNEL > FORCE_SUB_CHANNEL2:
